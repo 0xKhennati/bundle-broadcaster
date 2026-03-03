6c2cf406bb1fb2fc3261829e9578e737a0fe75ba
khennati mohamed <khennati@ymail.com>
2026-03-03 00:21:54 +0000

    v

--- strategies/relays/beaverbuild.go
+++ strategies/relays/beaverbuild.go
@@ -1,11 +1,16 @@
 package relays
 
 import (
+	"fmt"
+
 	"github.com/0xKhennati/bundle-broadcaster/strategies"
 )
 
 type BeaverbuildBuilder struct{}
 
 func (b *BeaverbuildBuilder) BuildRequest(bundle *strategies.IncomingBundle) (string, interface{}, error) {
-	return "eth_sendBundle", basePayload(bundle), nil
+	return "eth_sendBundle", map[string]interface{}{
+		"txs":         bundle.RawTxs,
+		"blockNumber": fmt.Sprintf("0x%x", bundle.TargetBlock),
+	}, nil
 }
